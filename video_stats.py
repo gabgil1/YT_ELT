6a4fb2af6339909b2441789b837f7d62381f068b
gabgil1 <gabigil245@gmail.com>
2025-12-07 17:44:54 -0300

    video id

--- video_stats.py
+++ video_stats.py
@@ -6,26 +6,24 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
-def get_playlistId():
+
+def get_playlist_Id():
 
     try:
 
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername={CHANNEL_HANDLE}&key={API_KEY}"
 
         response = requests.get(url)
-
         response.raise_for_status()
 
         data = response.json()
 
-        #print(json.dumps(data, indent=4))
-
         channel_items = data["items"][0]
-
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        print(channel_playlistId)
+        print("Playlist ID:", channel_playlistId)
 
         return channel_playlistId
     
@@ -33,5 +31,38 @@ def get_playlistId():
     except requests.exceptions.RequestException as e:
         raise e
 
+
+def get_videos_ids(playlistId):
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+    videos_ids = []
+    pageToken = None
+
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                videos_ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+            if not pageToken:
+                break
+
+        print("Videos IDs:", videos_ids)
+        return videos_ids
+
+    except requests.exceptions.RequestException as e: 
+        raise e
+
+
 if __name__ == "__main__":
-    get_playlistId()    
+    playlistId = get_playlist_Id()
+    print(get_videos_ids(playlistId))
